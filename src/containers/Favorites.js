041166408a73b580b150ae586f2de8cf2a6ba12d
Vera Chernushina <vera.chernushina@yandex.ru>
2022-02-25 19:12:25 +0300

    refactor: favorites products

--- src/containers/Favorites.js
+++ src/containers/Favorites.js
@@ -1,12 +1,27 @@
+import { useSelector } from 'react-redux';
 import FavoriteItem from '../components/Favorites/FavoriteItem';
 import './Products.css';
 
 const Favorites = () => {
-  return (
-    <div>
-      <FavoriteItem />
-    </div>
+  const favProducts = useSelector((state) =>
+    state.shop.products.filter((p) => p.isFavorite)
   );
+  let content = <p className="placeholder">Нет избранных товаров!</p>;
+  if (favProducts.length > 0) {
+    content = (
+      <ul className="products-list">
+        {favProducts.map((prod) => (
+          <FavoriteItem
+            key={prod.id}
+            id={prod.id}
+            title={prod.title}
+            description={prod.description}
+          />
+        ))}
+      </ul>
+    );
+  }
+  return content;
 };
 
 export default Favorites;
